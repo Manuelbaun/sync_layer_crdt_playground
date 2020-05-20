28a8d683b6280344242437e9482cdf99bfd53f9e
Manuel Baun <manuel.baun@gmail.com>
2020-05-20 11:53:58 +0200

    added atom encode to compare

--- test/measurenments/encoding.dart
+++ test/measurenments/encoding.dart
@@ -5,6 +5,7 @@ import 'package:sync_layer/types/index.dart';
 
 void main() {
   encodeCausalEntry();
+  encodeCausalEntryWithAtom();
 }
 
 void encodeCausalEntry() {
@@ -18,4 +19,24 @@ void encodeCausalEntry() {
   print(bytes);
   print(bytes.length);
   print(entryCopy);
+  print('----------------');
+}
+
+void encodeCausalEntryWithAtom() {
+  final id = Id(LogicalClock(1), 20);
+  final cause = Id(LogicalClock(0), 20);
+
+  final entry = CausalEntry(id, cause: cause, data: null);
+  final ts = HybridLogicalClock(DateTime(2020).millisecondsSinceEpoch, 0);
+
+  final atomId = Id(ts, 20);
+  final atom = Atom(atomId, 1, 'uuid', entry);
+
+  final bytes = msgpackEncode(atom);
+  final atomCopy = msgpackDecode(bytes);
+  print(entry);
+  print(bytes);
+  print(bytes.length);
+  print(atomCopy);
+  print('----------------');
 }
